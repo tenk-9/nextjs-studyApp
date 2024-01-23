81cc9a68e45b1a3db3036e5a5cf550ffbf0dd567
tenk9 <takuo6000@gmail.com>
2024-01-24 05:51:02 +0900

    mod: add log out

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,6 +11,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+
     const params = new URLSearchParams(searchParams);
     if (term) {
       params.set('query', term);
